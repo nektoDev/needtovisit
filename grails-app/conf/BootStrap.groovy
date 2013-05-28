878de75c486e7d483e6d89d682b97c5a2427bdc1
Tsykin V.A <ts.slawa@gmail.com>
2013-05-28 23:31:38 +0400

    fix bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -4,9 +4,9 @@ import ru.nektodev.needtovisit.User
 class BootStrap {
 
     def init = { servletContext ->
-        def userRole = SecRole.findByAuthority('ROLE_USER') ?: new Role(authority: 'ROLE_USER').save(failOnError: true)
-        def adminRole = SecRole.findByAuthority('ROLE_ADMIN') ?: new Role(authority: 'ROLE_ADMIN').save(failOnError: true)
-        def adminUser = SecUser.findByUsername('admin') ?: new User(
+        def userRole = Role.findByAuthority('ROLE_USER') ?: new Role(authority: 'ROLE_USER').save(failOnError: true)
+        def adminRole = Role.findByAuthority('ROLE_ADMIN') ?: new Role(authority: 'ROLE_ADMIN').save(failOnError: true)
+        def adminUser = User.findByUsername('admin') ?: new User(
                 username: 'admin',
                 password: springSecurityService.encodePassword('admin'),
                 enabled: true).save(failOnError: true)
