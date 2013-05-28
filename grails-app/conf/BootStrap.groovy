9332d0b9b7623dc6aa6101ac21ea2c173ae5386d
Tsykin V.A <ts.slawa@gmail.com>
2013-05-28 23:27:23 +0400

    Add generated data

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,20 @@
+import ru.nektodev.needtovisit.Role
+import ru.nektodev.needtovisit.User
+
 class BootStrap {
 
     def init = { servletContext ->
+        def userRole = SecRole.findByAuthority('ROLE_USER') ?: new Role(authority: 'ROLE_USER').save(failOnError: true)
+        def adminRole = SecRole.findByAuthority('ROLE_ADMIN') ?: new Role(authority: 'ROLE_ADMIN').save(failOnError: true)
+        def adminUser = SecUser.findByUsername('admin') ?: new User(
+                username: 'admin',
+                password: springSecurityService.encodePassword('admin'),
+                enabled: true).save(failOnError: true)
+
+        if (!adminUser.authorities.contains(adminRole)) {
+            SecUserSecRole.create adminUser, adminRole
+        }
+
     }
     def destroy = {
     }
