177ffbe4631a98508855796bd987e9b4aa6bb072
Tsykin V.A <ts.slawa@gmail.com>
2013-06-02 00:49:51 +0400

    add place to header

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,7 @@
+import ru.nektodev.needtovisit.Place
 import ru.nektodev.needtovisit.Role
+import ru.nektodev.needtovisit.Users
 import ru.nektodev.needtovisit.UsersRole
-import ru.nektodev.needtovisit.Users
-import ru.nektodev.needtovisit.Users
 
 class BootStrap {
     def springSecurityService;
@@ -17,7 +17,7 @@ class BootStrap {
             UsersRole.create adminUser, adminRole
         }
 
-        def zoo = Place.findByName("ZOO") ?: new Place(name: 'ZOO').addToUsers(adminUser).save();
+        def zoo = Place.findByName("ZOO") ?: new Place(name: 'ZOO')save();
 
     }
     def destroy = {
