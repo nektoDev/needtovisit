09b6353d7735650b59ae0c98f43553c4a8fd9d9e
Tsykin V.A <ts.slawa@gmail.com>
2013-06-20 09:45:34 +0400

    addUserPlaceRelation

--- grails-app/controllers/ru/nektodev/needtovisit/IndexController.groovy
+++ grails-app/controllers/ru/nektodev/needtovisit/IndexController.groovy
@@ -36,4 +36,21 @@ class IndexController {
         index()
         render(view: 'index')
     }
+
+    def addUserPlaceRelation() {
+        if (springSecurityService.isLoggedIn()) {
+            Users user = springSecurityService.currentUser as Users;
+            def p_id = params.get("place") as String;
+
+            if (p_id != null && !p_id.isEmpty()) {
+
+                Place p = Place.get(p_id as Long);
+                if (p != null){
+                    UserPlaceRelation.findByUserAndPlace(user as Users, p) ?: new UserPlaceRelation(user: user, place: p).save(flush: true, insert: true);
+                }
+
+            }
+        index();
+        }
+    }
 }
