3458b4d378de4f76d6a188e74c931d3bc4aa09d6
Tsykin V.A <ts.slawa@gmail.com>
2013-06-24 12:38:22 +0400

    implement methods in PlaceService

--- grails-app/domain/ru/nektodev/needtovisit/Place.groovy
+++ grails-app/domain/ru/nektodev/needtovisit/Place.groovy
@@ -17,34 +17,46 @@ class Place {
 
     }
 
-    static List<Place> listByUser(Users u) {
+    static List<Place> listByUser(Users u, Long max = 20) {
 
         def result = createCriteria().list {
             userRelation {
                 eq("user", u)
             }
+            maxResults(max)
         }
         return result
     }
 
-    static List<Place> listByUserNotEqual(Users u) {
+    static List<Place> listByUserNotEqual(Users u, Long max = 10) {
 
         def result = executeQuery(
                     """SELECT DISTINCT p FROM Place p JOIN FETCH p.userRelation ur
                         WHERE NOT EXISTS
                              (FROM p.userRelation ur
-                                 WHERE ur.user = :user)""", [user: u])
+                                 WHERE ur.user = :user)""", [user: u], [max: max])
 
         return result
     }
 
-    static List<Place> listByUserEqual(Users u) {
+    static List<Place> listNotVisitedByUser(Users u, Long max = 10) {
 
         def result = executeQuery(
                 """SELECT DISTINCT p FROM Place p JOIN FETCH p.userRelation ur
                         WHERE EXISTS
                              (FROM p.userRelation ur
-                                 WHERE ur.user = :user)""", [user: u])
+                                 WHERE ur.user = :user AND NOT ur.visited)""", [user: u], [max: max])
+
+        return result
+    }
+
+    static List<Place> listVisitedByUser(Users u, Long max = 10) {
+
+        def result = executeQuery(
+                """SELECT DISTINCT p FROM Place p JOIN FETCH p.userRelation ur
+                        WHERE EXISTS
+                             (FROM p.userRelation ur
+                                 WHERE ur.user = :user AND ur.visited)""", [user: u], [max: max])
 
         return result
     }
--- grails-app/services/needtovisit/PlaceListsService.groovy
+++ grails-app/services/needtovisit/PlaceListsService.groovy
@@ -1,10 +1,51 @@
 package needtovisit
 
+import ru.nektodev.needtovisit.Place
+import ru.nektodev.needtovisit.Users
+
 class PlaceListsService {
 
+    List<Place> getPlacesNewList(Users u, Long max = 10) {
+        List<Place> result = new ArrayList<>();
 
+        if (u != null) {
+            result = Place.listByUserNotEqual(u, max) as List<Place>;
+        }
 
-    def serviceMethod() {
+        return result;
+    }
 
+    List<Place> getPlacesToVisitList(Users u, Long max = 10) {
+        List<Place> result = new ArrayList<>();
+
+        if (u != null) {
+            result = Place.listNotVisitedByUser(u, max) as List<Place>;
+        }
+
+        return result;
+    }
+
+    List<Place> getPlacesVisitedList(Users u, Long max = 10) {
+        List<Place> result = new ArrayList<>();
+
+        if (u != null) {
+            result = Place.listVisitedByUser(u, max) as List<Place>;
+        }
+
+        return result;
+    }
+    
+    List<Place> getPlacesList(Long max = 10) {
+        return Place.list()
+    }
+    
+    List<Place> getPlacesList(Users u, Long max = 20) {
+        List<Place> result = new ArrayList<>();
+
+        if (u != null) {
+            result = Place.listByUser(u, max) as List<Place>;
+        }
+
+        return result;
     }
 }
