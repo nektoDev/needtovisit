c38891ffd5110f29bb8caca70eb520aee29b60b8
Tsykin V.A <ts.slawa@gmail.com>
2013-06-24 15:12:07 +0400

    add test for placelistservice

---
+++ test/unit/needtovisit/PlaceListsServiceTests.groovy
@@ -0,0 +1,13 @@
+package needtovisit
+
+import grails.test.mixin.TestFor
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(PlaceListsService)
+class PlaceListsServiceTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
