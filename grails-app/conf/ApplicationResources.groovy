3c8d9dd0fa4e8c399f1f25c6756f90f533818968
Tsykin V.A <ts.slawa@gmail.com>
2013-06-27 15:39:56 +0400

    needtovisit-8 Поле пароля. Плагин для парольных полей
    Добавлен плагин

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -2,12 +2,18 @@ modules = {
     application {
         resource url:'js/application.js'
         resource url:'js/index.js'
-        resource url:'js/jquery-ui-1.10.3.custom.js'
+        resource url:'js/plugins/jquery-ui-1.10.3.custom.js'
+    }
+
+    passfield {
+        resource url: 'js/plugins/passfield.min.js'
+        resource url: 'css/plugins/passfield.min.css'
 
     }
+
     css {
         resource url: 'css/skeleton.css'
-        resource url: 'css/jquery-ui-1.10.3.custom.css'
+        resource url: 'css/plugins/jquery-ui-1.10.3.custom.css'
     }
 
     login {
