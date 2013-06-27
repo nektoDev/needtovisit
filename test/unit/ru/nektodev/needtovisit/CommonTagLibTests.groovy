19d010c76c94042b2ace0a5f1f92d09e73e565d6
Tsykin V.A <ts.slawa@gmail.com>
2013-06-27 18:08:06 +0400

    Add test

---
+++ test/unit/ru/nektodev/needtovisit/CommonTagLibTests.groovy
@@ -0,0 +1,11 @@
+package ru.nektodev.needtovisit
+
+import grails.test.mixin.TestFor
+/**
+ * See the API for {@link grails.test.mixin.web.GroovyPageUnitTestMixin} for usage instructions
+ */
+@TestFor(CommonTagLib)
+class CommonTagLibTests {
+
+
+}
